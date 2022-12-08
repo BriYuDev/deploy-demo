02b39031bed3aa7bcb58919e485590bc38b21817
BriYuDev <aesblox12@gmail.com>
2022-12-08 12:54:17 +0700

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MAMANG HAS BEEN DEPLOYED
+          MAMANG HAS BEEN DEPLOYED CORRECTLY!!!
         </p>
       </header>
     </div>
